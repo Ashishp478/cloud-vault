99271bf5d5f3078b0c3d13618b5ffa4a1d42b70b
Ashish Prasad <prasadashish478@gmail.com>
2026-05-17 13:35:49 +0530

    Fix PORT variable issue

--- backend/index.js
+++ backend/index.js
@@ -44,12 +44,10 @@ app.use((err,req,res,next)=>{
     });
 });
 
-//const PORT = process.env.PORT || 5000;
-
-//app.listen(PORT,()=>{
-  // console.log(`Server running on ${PORT}`);
-//});
+// Debug
 console.log("Gemini Key Exists:", !!process.env.GEMINI_API_KEY);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
