9bffb38f54571c709dc74a403e260ae8128cb5f5
Ashish Prasad <prasadashish478@gmail.com>
2026-05-17 12:25:30 +0530

    Fix Render

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env'),
+  override: true
+});
+
+connectDB();
 
 const authRoutes = require('./routes/authRoutes');
 const fileRoutes = require('./routes/fileRoutes');
@@ -12,40 +16,36 @@ const folderRoutes = require('./routes/folderRoutes');
 
 const app = express();
 
-connectDB();
-
-// Middleware
-app.use(
-  cors({
+// CORS
+app.use(cors({
     origin: [
-      "http://cloudvault-storage-ashish28.s3-website.eu-north-1.amazonaws.com",
-      "http://localhost:5173"
+        "http://cloudvault-storage-ashish28.s3-website.eu-north-1.amazonaws.com",
+        "http://localhost:5173"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
-  })
-);
+}));
 
 app.use(express.json());
 
-// Health route (testing)
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+// Test route
+app.get('/', (req,res)=>{
+   res.send('Backend is running 🚀');
 });
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/folders', folderRoutes);
 
 // Error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Server Error' });
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).json({
+        message: err.message
+    });
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+   console.log(`Server running on ${PORT}`);
 });
